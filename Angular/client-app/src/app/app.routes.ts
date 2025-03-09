5e79bde5d114a86b2a0644219cb826298a0dd1e2
AkumaOff <142070440+1234sleepy@users.noreply.github.com>
2025-03-09 08:38:42 -0800

    front

--- Angular/client-app/src/app/app.routes.ts
+++ Angular/client-app/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './components/main/main.component';
+import { CatalogComponent } from './components/catalog/catalog.component';
 
 export const routes: Routes = [
     {path: '', pathMatch: 'full', redirectTo: 'main'},
-    {path: 'main', component: MainComponent}
+    {path: 'main', component: MainComponent},
+    {path: 'catalog', component: CatalogComponent}
 ];
