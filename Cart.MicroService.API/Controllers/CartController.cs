ff33048dedfebd26bdf8d4788054b4cb11ae0abd
Artur Yoltukhivskyi <1234sleepyofficial@gmail.com>
2025-12-07 09:10:21 -0800

    add

---
+++ Cart.MicroService.API/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using Cart.MicroService.Domain.UseCases.CreateCart;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cart.MicroService.API.Controllers;
+
+[ApiController, Route("api/cart")]
+public class CartController(IMediator mediator) : ControllerBase
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpPost]
+    public async Task<ActionResult> UpdateCart([FromBody] UpdateCartCommand model,
+    CancellationToken cancellationToken)
+    {
+        await _mediator.Send(model, cancellationToken);
+        return Ok();
+    }
+}
---
+++ Cart.MicroService.API/Program.cs
@@ -0,0 +1,29 @@
+using Cart.MicroService.Storage.DependencyInjection;
+using Cart.MicroService.Domain.DependencyInjection;
+using Cart.MicroService.Storage;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddControllers();
+builder.Services.AddDomain();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddStorage(builder.Configuration.GetConnectionString("Postgres")!);
+
+
+var app = builder.Build();
+
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    scope.ServiceProvider.GetRequiredService<DataContext>().Database.Migrate();
+}
+
+app.Run();
